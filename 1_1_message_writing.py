138581e07804d2f8a0ea5315b21941314783e70a
mauurao <geralxk@gmail.com>
2021-08-13 23:56:31 +0100

    Test

--- 1_1_message_writing.py
+++ 1_1_message_writing.py
@@ -2,6 +2,7 @@
 To start, it is always the basics and it will always be the famous "Hello World".
 This program will print the message "Welcome to python", preceded by a blank line.
 
+
 '''
 
 if __name__ == '__main__':
