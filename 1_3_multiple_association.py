06a1989169f14b6c46aa55570a08c7b29bfadfee
mauurao <geralxk@gmail.com>
2021-08-14 00:45:39 +0100

    main

---
+++ 1_3_multiple_association.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+"""
+This program assigns values to multiple variables and prints them
+"""
+
+if __name__ == '__main__':
+    name, local, age="José Costa", "Lisbon", 30
+    print(name,local,age)
