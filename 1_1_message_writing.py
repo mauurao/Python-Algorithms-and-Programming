3da70bff4cbf6a75a42491ef1ff2a707e6f56ba2
mauurao <geralxk@gmail.com>
2021-08-14 00:28:09 +0100

    main

--- 1_1_message_writing.py
+++ 1_1_message_writing.py
@@ -1,9 +1,9 @@
-'''
+#!/usr/bin/python
+"""
 To start, it is always the basics and it will always be the famous "Hello World".
 This program will print the message "Welcome to python", preceded by a blank line.
 
-
-'''
+"""
 
 if __name__ == '__main__':
         print("\n Welcome to python")
---
+++ 1_2_variable_atribution.py
@@ -0,0 +1,9 @@
+"""
+This program will print the message "Welcome to python" 
+but the message will be assigned to a variable, preceded by a blank line.
+
+"""
+
+if __name__ == '__main__':
+    message="José, Welcome to Python!"
+    print(message)
